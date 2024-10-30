2b801361857b23906f4ab8362738b36f233589f3
1965321 <1965321@01-18105.pusd.dom>
2024-10-30 10:21:02 -0700

    have 1 corner, has bug with top right corner

--- Loops + Drawing/src/Runner.java
+++ Loops + Drawing/src/Runner.java
@@ -14,51 +14,38 @@ public class Runner extends JPanel{
 	public void paint(Graphics g) {
 		
 		Graphics2D g2 = (Graphics2D) g;
-        g2.setStroke(new BasicStroke(5));
+        g2.setStroke(new BasicStroke(1));
+        int x = 0;
+        int y = 650;
+        while(x <= 1000 && y > 0) {
+        g2.drawLine(0, y, x, 0);
+        x+=25;
+        y-=25;
+       
+        }
+        int x2 = 325;
+        int y2 = 0;
+        while(x2 <= 1000 && y2 < 650) {
+        g2.drawLine(x2, 0, 1000, y2);
+        x2+=25;
+        y2+=25;
+       
+        }
         
-		 
-		//1) draw a concentric rectangle that's 10pixels from all sides of the "gray"
-		//portion of the GUI
-		g2.drawRect(10, 10, 765, 540);
+        for(int x3 = 0, y3 = 0; x3 <= 1000 && y3 < 0; x3+=25, y3+=25) {
+        	g2.drawLine(0, 0, x3, y3);
+        }
+        
+        
+        
+        
 		
-		
-		//2) Draw a horizontal line to bisect the Rectangle
-		g2.drawLine(10, 270, 770, 270);
-		
-		
-		//3) Draw a vertical line to bisect the shape again
-		g2.drawLine(765/2, 10, 765/2,550 );
-		
-		
-		//4) small rectangles on top-left
-		g2.drawLine(765/4, 10, 765/4, 550/2-10);
-		
-		
-		/* 
-		 * int var = 0; 						<---- control variable
-		 * while(var < 10){ 					<---- condition to run the loop
-		 *   
-		 *   //any repeating code
-		 *   
-		 *   var++;								<---- update control variable
-		 *   
-		 * }
-		 */
-		
-		 
-
+	
 	
 	
 	}
-	
-	
-	
-	
-	
-	
-	
-	
-	
+
+
 	public static void main(String[] arg) {
 		Runner m = new Runner();
 	}
@@ -66,7 +53,7 @@ public class Runner extends JPanel{
 	public Runner() {
 		JFrame f = new JFrame("Method Use");
 		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		f.setSize(800,600);
+		f.setSize(1000,650);
 		f.add(this);
 		f.setVisible(true);
 		
